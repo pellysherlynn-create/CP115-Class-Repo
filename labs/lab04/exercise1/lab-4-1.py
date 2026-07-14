25639fcfdbe78e11753f0ac64ff3be14af996876
pellysherlynn-create <pellysherlynn@gmail.com>
2026-07-14 11:26:34 +0800

    PEMBETULAN2

--- labs/lab04/exercise1/lab-4-1.py
+++ labs/lab04/exercise1/lab-4-1.py
@@ -1,9 +1,9 @@
-kwh = int(input())
+kwh = float(input())
 if kwh <= 100:
     totalBill = kwh * 0.3
 else:
     if kwh <= 200:
-        totalBill = kwh * 0.5
+        totalBill = 30.0 + ((kwh - 100 ) * 0.5)
     else:
-        totalBill = kwh * 0.25
+        totalBill = 80.0 + ((kwh - 200) * 0.75)
 print(totalBill)
